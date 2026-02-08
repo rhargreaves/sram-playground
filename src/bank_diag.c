4af4900cc404e8fb3a2a777478c70a5ee88e7e7c
Robert Hargreaves <kiyouta@gmail.com>
2026-02-08 20:03:59 +0000

    feat: test near & far banks

--- src/bank_diag.c
+++ src/bank_diag.c
@@ -1,16 +1,80 @@
 #include <genesis.h>
+#include "config.h"
 #include "data.h"
+#include "mapper.h"
+#include "vdp.h"
+#include "vdp_bg.h"
+
+static const void* NEAR_BANKS[] = {
+    data1,
+    data2,
+    data3,
+    data4,
+    data5,
+    data6,
+    data7,
+};
+
+static const void* FAR_BANKS[] = { data8, data9, data10 };
+
+static void print_pass(void)
+{
+    VDP_setTextPalette(PAL2);
+    CON_write("Pass\n");
+    VDP_setTextPalette(PAL0);
+}
+
+static void print_fail(void)
+{
+    VDP_setTextPalette(PAL1);
+    CON_write("Fail\n");
+    VDP_setTextPalette(PAL0);
+}
+
+static void print_result(bool res)
+{
+    if (res) {
+        print_pass();
+    } else {
+        print_fail();
+    }
+    // SYS_getBank(u16 regionIndex)
+}
+
+static void test_bank_at(const void* addr)
+{
+    CON_write("Near %06lX: ", (u32)addr);
+    print_result(*((char*)addr) == 0x31);
+}
+
+static void test_bank_invalid_at(const void* addr)
+{
+    CON_write("Near invalid %06lX: ", (u32)addr);
+    print_result(*((char*)addr) != 0x31);
+}
+
+static void test_far_bank_at(const void* addr)
+{
+    CON_write("Far %06lX: ", (u32)addr);
+    print_result(*((char*)FAR(addr)) == 0x31);
+}
 
 void test_banks(void)
 {
     CON_write("Bank Test Program\n-----------------\n");
-    CON_write("test_data1 = 0x%x\n", data1[0]);
-    CON_write("test_data2 = 0x%x\n", data2[0]);
-    CON_write("test_data3 = 0x%x\n", data3[0]);
-    CON_write("test_data4 = 0x%x\n", data4[0]);
-    CON_write("test_data5 = 0x%x\n", data5[0]);
-    CON_write("test_data6 = 0x%x\n", data6[0]);
-    CON_write("test_data7 = 0x%x\n", data7[0]);
-    CON_write("test_data8 = 0x%x\n", ((u8*)FAR(data8))[0]);
+    CON_write("ENABLE_BANK_SWITCH: %i\n", ENABLE_BANK_SWITCH);
+
+    for (int i = 0; i < 7; i++) {
+        test_bank_at(NEAR_BANKS[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        test_bank_invalid_at(FAR_BANKS[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        test_far_bank_at(FAR_BANKS[i]);
+    }
+
     CON_write("Done");
 }
--- src/main.c
+++ src/main.c
@@ -2,19 +2,9 @@
 #include "sram_diag.h"
 #include "bank_diag.h"
 
-const __attribute__((externally_visible)) vu8 padding[2500000] = { 1 };
-
-#if (ENABLE_BANK_SWITCH != 1)
-#error Enable bank switching in SGDK lib
-#endif
-
 int main()
 {
-    (void)padding;
-
     CON_reset();
-    CON_setConsoleSize(0, 0, 40, 28);
-
     SYS_disableInts();
     Z80_requestBus(TRUE);
 
